049bd81a4eb3c589ca6783db80b2f75fe90815ba
Crimson-Amir <ytchanneltinos@gmail.com>
2024-05-27 11:37:50 +0330

    asynchrnous-virtualizor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from utilities import handle_error, replace_with_space, report_problem_to_admin, human_readable, unix_time_to_datetime
-import requests
+from utilities import handle_error
 from datetime import datetime
 from database import create_database
 create_database('virtualizor')
@@ -9,9 +8,10 @@ from telegram.ext import (ApplicationBuilder, ContextTypes, CommandHandler, Conv
                           CallbackQueryHandler)
 from private import telegram_bot_token, ADMIN_CHAT_ID
 from sqlite_manager import ManageDb
-import pytz
+from vrtualizorApi import run
 
-check_every_min = 1
+
+check_every_min = 5
 END_POINT, API_KEY, API_PASS = range(3)
 
 bandwidth_notification_text = '🔔 [bandWidth Notification] You have consumed {0}% of virtual server {1} bandwidth!\nRemaining traffic: {2} GB'
@@ -24,72 +24,6 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
-
-
-def sort_data(json_file, special_vps=None, get_detail=False, get_vs_usage_detail=False):
-    final_lines, vps_name, usage_detail, vps_info = [], [], {}, []
-
-    for vps_id, vps_data in json_file.get('vs', {}).items():
-        if special_vps and str(special_vps) != vps_id: continue
-        final_lines.append(f'\nvirtual server {vps_id}:')
-        vps_name.append(vps_id)
-
-        registare_time = unix_time_to_datetime(int(vps_data.get('time')))
-        used_band = float(vps_data.get("used_bandwidth"))
-        total_band = float(vps_data.get("bandwidth"))
-        band_precent = round((used_band / total_band) * 100, 2)
-        time_difference = datetime.now(pytz.timezone('Asia/Tehran')).replace(tzinfo=None) - registare_time
-        left_band = round(total_band - used_band, 2)
-
-        if get_vs_usage_detail:
-            usage_detail[vps_id] = {'bandwidth_left': band_precent, 'registare_to_now': time_difference.days, 'left_band': left_band}
-        elif get_detail:
-            vps_info = [f'{replace_with_space(key)} : {values}' for key, values in vps_data.items()]
-        else:
-            detail = (f'\nHost Name: {vps_data.get("hostname")}'
-                      f'\nBand Width: {used_band}/{total_band} GB ({band_precent}%)'
-                      f'\nLeft BandWidth: {left_band} GB'
-                      f'\nRegistration: {registare_time} ({human_readable(registare_time)} - {time_difference.days} day)'
-                      f'\nOS Name: {vps_data.get("os_name")}'
-                      f'\nOS Distro: {vps_data.get("os_distro")}'
-                      f'\nCPU Cores: {vps_data.get("cores")}'
-                      f'\nNetwork Speed: {vps_data.get("network_speed")}'
-                      f'\nRam: {vps_data.get("ram")}'
-                      f'\nSpace: {vps_data.get("space")} GB'
-                      f'\nIPs: {", ".join(vps_data.get("ips").values())}')
-            vps_info = [detail]
-
-        final_lines.extend(vps_info)
-
-    final_lines.extend(["\n\n• Select the desired VS to view details ⤵"])
-    return '\n'.join(final_lines), vps_name, usage_detail
-
-
-def get_result_from_api(end_point, api_key, api_pass, act='listvs', special_vps=None, get_detail=False, get_vs_usage_detail=False):
-
-    url = f"{end_point}/index.php"
-    params = {
-        'act': act,
-        'api': 'json',
-        'apikey': api_key,
-        'apipass': api_pass
-    }
-
-    try:
-        with requests.get(url, params=params, verify=False, timeout=5) as response:
-            if response.ok:
-                connection_response_json = response.json()
-                connection_response = sort_data(connection_response_json, special_vps=special_vps, get_detail=get_detail, get_vs_usage_detail=get_vs_usage_detail)
-                logging.info('connect Success!')
-                return 'OK', *connection_response
-            else:
-                logging.info('connect Failed!')
-                return f'error code {response.status_code}', response, [], []
-
-
-    except Exception as e:
-        report_problem_to_admin(f'Error In func [get_result_from_api]\nerror Type:{type(e)}\nreason: {str(e)}')
-        return f'exception error {type(e)}', [], []
 
 
 @handle_error
@@ -148,10 +82,10 @@ async def get_api_pass(update: Update, context: ContextTypes.DEFAULT_TYPE):
     api_key = context.user_data['api_key']
     end_point = context.user_data['end_point']
 
-    get_result = get_result_from_api(end_point, api_key, api_pass)
+    get_result = run(end_point, api_key, api_pass)
 
-    if get_result[0] == 'OK':
-        keyboard = [[InlineKeyboardButton(f'virtual server {vs}', callback_data=vs)] for vs in get_result[2]]
+    if get_result[0]:
+        keyboard = [[InlineKeyboardButton(f'virtual server {vs}', callback_data=vs)] for vs in get_result[0][1]]
         text = f"<b>Done! You can view your account details:\n\n{get_result[1]}</b>"
 
         sqlite_manager.insert(table='API_DETAIL',
@@ -202,11 +136,11 @@ async def main_menu(update: Update, context: ContextTypes.DEFAULT_TYPE):
     api_data = [(data[2], data[3], data[4]) for data in get_data]
 
     for end_point, api_key, api_pass in api_data:
-        get_result = get_result_from_api(end_point, api_key, api_pass, special_vps=special_vps, get_detail=get_detail)
+        get_result = run(end_point, api_key, api_pass, special_vps=special_vps, get_detail=get_detail)
 
-        if get_result[0] == 'OK':
-            keyboard.extend([[InlineKeyboardButton(f'virtual server {vs}', callback_data=f'main_menu{vs}')] for vs in get_result[2]])
-            text += f"{get_result[1]}"
+        if get_result[0]:
+            keyboard.extend([[InlineKeyboardButton(f'virtual server {vs}', callback_data=f'main_menu{vs}')] for vs in get_result[0][1]])
+            text += f"{get_result[0]}"
         else:
             keyboard.append([InlineKeyboardButton('Main Menu', callback_data='main_menu')])
             text += "<b>sorry, something went wrong!</b>"
@@ -234,9 +168,9 @@ async def notification_job(context: ContextTypes.DEFAULT_TYPE):
         period_notification_day = get_user_notif_setting[0][1]
         left_traffic_gb = get_user_notif_setting[0][2]
 
-        get_result = get_result_from_api(end_point, api_key, api_pass, get_vs_usage_detail=True)
-        if get_result[0] == 'OK':
-            for vs_id, details in get_result[3].items():
+        get_result = run(end_point, api_key, api_pass, get_vs_usage_detail=True)
+        if get_result[0]:
+            for vs_id, details in get_result[2].items():
                 check_notif = [(search[3], search[4], search[6]) for search in get_all_notification if search[2] == chat_id and search[1] == int(vs_id)]
 
                 if not check_notif:
@@ -263,12 +197,12 @@ async def notification_job(context: ContextTypes.DEFAULT_TYPE):
                 elif check_notif[0][1] and registare_to_now < period_notification_day:
                     sqlite_manager.update({'VS_NOTIFICATION': {'notification_day': 0}}, where=f'vps_id = {vs_id}')
 
-                if left_band <= left_traffic_gb and not check_notif[0][2]:
+                if left_band <= float(left_traffic_gb) and not check_notif[0][2]:
                     sqlite_manager.update({'VS_NOTIFICATION': {'notification_traffic': 1}}, where=f'vps_id = {vs_id}')
                     text = traffic_notification_text.format(left_band, vs_id)
                     await context.bot.send_message(text=text, chat_id=chat_id)
 
-                elif check_notif[0][2] and left_band > left_traffic_gb:
+                elif check_notif[0][2] and left_band > float(left_traffic_gb):
                     sqlite_manager.update({'VS_NOTIFICATION': {'notification_traffic': 0}}, where=f'vps_id = {vs_id}')
 
 
---
+++ vrtualizorApi.py
@@ -0,0 +1,143 @@
+import aiohttp
+import asyncio
+from utilities import replace_with_space, human_readable, unix_time_to_datetime
+import pytz
+from datetime import datetime
+
+
+class SortData:
+    @staticmethod
+    async def sort_list_vs(json_file, special_vps=None, get_detail=False, get_vs_usage_detail=False):
+        final_lines, vps_name, usage_detail, vps_info = [], [], {}, []
+        vps_items = json_file.get('vs', {}).items()
+
+        for vps_id, vps_data in vps_items:
+
+            if special_vps and str(special_vps) != vps_id:
+                continue
+
+            final_lines.append(f'\nVirtual server {vps_id}:')
+            vps_name.append(vps_id)
+
+            register_time = unix_time_to_datetime(int(vps_data.get('time')))
+            used_band = float(vps_data.get("used_bandwidth"))
+            total_band = float(vps_data.get("bandwidth"))
+            band_percent = round((used_band / total_band) * 100, 2)
+            time_difference = datetime.now(pytz.timezone('Asia/Tehran')).replace(tzinfo=None) - register_time
+            left_band = round(total_band - used_band, 2)
+
+            if get_vs_usage_detail:
+                usage_detail[vps_id] = {
+                    'bandwidth_left': band_percent,
+                    'register_to_now': time_difference.days,
+                    'left_band': left_band
+                }
+
+            elif get_detail:
+                vps_info = [f'{replace_with_space(key)} : {values}' for key, values in vps_data.items()]
+
+            else:
+                details = (
+                    f'\nHost Name: {vps_data.get("hostname")}'
+                    f'Band Width: {used_band}/{total_band} GB ({band_percent}%)'
+                    f'Left BandWidth: {left_band} GB'
+                    f'Registration: {register_time} ({human_readable(register_time)} - {time_difference.days} day(s))'
+                    f'OS Name: {vps_data.get("os_name")}'
+                    f'OS Distro: {vps_data.get("os_distro")}'
+                    f'CPU Cores: {vps_data.get("cores")}'
+                    f'Network Speed: {vps_data.get("network_speed")}'
+                    f'Ram: {vps_data.get("ram")}'
+                    f'Space: {vps_data.get("space")} GB'
+                    f'IPs: {", ".join(vps_data.get("ips").values())}'
+                )
+                vps_info = [details]
+
+            final_lines.extend(vps_info)
+
+        final_lines.append("\n\n• Select the desired VS to view details ⤵")
+
+        return '\n'.join(final_lines), vps_name, usage_detail
+
+
+
+class SessionError(Exception):
+    def __init__(self, message=None):
+        super().__init__(message)
+
+
+class MakeRequest:
+    def __init__(self, session):
+        self._session = session
+        print('session set success!')
+
+    async def make_request(self, url, params=None):
+        if not self._session:
+            raise SessionError("session is not set!")
+
+        async with self._session.get(url, params=params, ssl=False, timeout=10) as response:
+            try:
+                response.raise_for_status()
+                return await response.json(content_type=None)
+            except aiohttp.ContentTypeError:
+                response_text = await response.text()
+                print(f"Failed to decode JSON. Response text: {response_text}")
+                return None
+            except aiohttp.ClientResponseError as e:
+                print(f"Request failed with status {e.status}: {e.message}")
+                return None
+
+
+class RequestFactory:
+    @staticmethod
+    async def run_requests(requests_list, params=None):
+        async with aiohttp.ClientSession() as session:
+            request_manager = MakeRequest(session)
+            requests_ = [request_manager.make_request(request, params=params) for request in requests_list]
+            results = await asyncio.gather(*requests_, return_exceptions=True)
+            return results
+
+class Virtualizor:
+    @staticmethod
+    async def execute_act(end_points_list, api_key, api_pass, act='listvs'):
+
+        url = "{0}/index.php"
+        list_of_url = [url.format(url_format) for url_format in list(end_points_list)]
+
+        params = {
+            'act': act,
+            'api': 'json',
+            'apikey': api_key,
+            'apipass': api_pass
+        }
+
+        request = RequestFactory()
+        make_request = await request.run_requests(list_of_url, params=params)
+        return make_request
+
+
+class AbstractVirtualizorFactory:
+    def __init__(self, **kwargs):
+        self._sort_class = SortData()
+        self._argument = kwargs
+
+
+class FactoryListVs(AbstractVirtualizorFactory):
+    async def execute(self, result_list):
+        sort_list_data = [self._sort_class.sort_list_vs(result, **self._argument) for result in result_list if isinstance(result, dict)]
+        return await asyncio.gather(*sort_list_data)
+
+
+async def run_code(endpoint, api_key, api_pass, **kwargs):
+    a = Virtualizor()
+    get_result = await a.execute_act(endpoint, api_key, api_pass)
+    sort_list = FactoryListVs(**kwargs)
+    return await sort_list.execute(get_result)
+
+
+def run(endpoint, api_key, api_pass, special_vps=None, get_detail=None, get_vs_usage_detail=None):
+    return asyncio.run(run_code(endpoint, api_key, api_pass,
+                                special_vps=special_vps, get_detail=get_detail, get_vs_usage_detail=get_vs_usage_detail))
+
+
+a = run(['https://185.215.231.72:4083'], 'FAQPKPC9GPUJNK84', 'DozkjalXanJStE2bcli2Q6wvjAEIT1Fa')
+print(a)
