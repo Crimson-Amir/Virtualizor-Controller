98d9a50b9ba794e942de19d3777979aa4f442e92
Crimson-Amir <ytchanneltinos@gmail.com>
2024-06-15 11:22:55 +0330

    update-req.txt

--- virtualizorApi.py
+++ virtualizorApi.py
@@ -68,20 +68,16 @@ class SessionError(Exception):
 class MakeRequest:
     def __init__(self, session):
         self._session = session
-        print('session set success!')
 
-    async def make_request(self, url, params=None):
+    async def make_request(self, url, headers=None, params=None):
         if not self._session:
             raise SessionError("session is not set!")
 
-        async with self._session.get(url, params=params, ssl=False, timeout=10) as response:
+        async with self._session.get(url, headers=headers, params=params, ssl=False, timeout=10) as response:
             try:
                 response.raise_for_status()
                 return await response.json(content_type=None)
-            except aiohttp.ContentTypeError:
-                response_text = await response.text()
-                print(f"Failed to decode JSON. Response text: {response_text}")
-                return None
+
             except aiohttp.ClientResponseError as e:
                 print(f"Request failed with status {e.status}: {e.message}")
                 return None
@@ -89,11 +85,14 @@ class MakeRequest:
 
 class RequestFactory:
     @staticmethod
-    async def run_requests(requests_list, params=None):
-        async with aiohttp.ClientSession() as session:
+    async def run_requests(requests_list, session_header=None, headers=None, params=None, return_exceptions=True):
+        if not isinstance(requests_list, list):
+            requests_list = [requests_list]
+
+        async with aiohttp.ClientSession(headers=session_header) as session:
             request_manager = MakeRequest(session)
-            requests_ = [request_manager.make_request(request, params=params) for request in requests_list]
-            results = await asyncio.gather(*requests_, return_exceptions=True)
+            requests_ = [request_manager.make_request(request, headers=headers, params=params) for request in requests_list]
+            results = await asyncio.gather(*requests_, return_exceptions=return_exceptions)
             return results
 
 class Virtualizor:
